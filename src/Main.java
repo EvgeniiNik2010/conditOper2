c4b1113747a5aed61fc5bfd32f80317d36b57855
EvgeniiNik2010 <eurostart@gmail.com>
2024-11-11 10:00:43 +0700

    в

--- src/Main.java
+++ src/Main.java
@@ -5,12 +5,11 @@ public class Main {
         int clientOS = 0;
         if (clientOS == 1) {
             System.out.println("Установите версию приложения для Android по ссылке");
-        } else {
-            if (clientOS == 0) {
-                System.out.println("Установите версию приложения для iOS по ссылке");
-            }else
-                System.out.println("Ничего не установить");
-        }
+        } else
+            if (clientOS == 0)
+            System.out.println("Установите версию приложения для iOS по ссылке");
+
+
 
         System.out.println("Задача 2");
 
